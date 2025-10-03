7d446d5290d169874404cd4b4caea46983faba67
harshadbokphode <harshadbokphode21@gmail.com>
2025-10-03 14:42:32 +0530

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,66 +1,46 @@
+import path from "path";
 import express from "express";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import cors from "cors";
-
 import notesRoutes from "./routes/notesRoutes.js";
-import {connectDB}  from "./config/db.js";
+import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
-
 
 dotenv.config();
 
-
 const app = express();
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
-app.use(cors({
-  origin: "http://localhost:5173"
-})
-);
+// CORS in dev
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors({
+    origin: "http://localhost:5173"
+  }));
+}
 
 app.use(express.json());
 app.use(rateLimiter);
 
+// API routes
+app.use("/api/notes", notesRoutes);
 
-//app.use((req,res,next) => {
- // console.log(`Req method is ${req.method} and req url is ${req.url}`);
- // next();
- // });
+// Serve frontend in production
+if (process.env.NODE_ENV === "production") {
+  // go up 2 levels from backend/src to reach frontend/dist
+  const frontendPath = path.join(__dirname, "../frontend/dist");
 
-app.use("/api/notes" , notesRoutes);
+  app.use(express.static(frontendPath));
 
-connectDB().then(()=> {
-  
-app.listen(PORT, () => {
-    console.log("server started on PORT: ", PORT);
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname,"../frontend","dist", "index.html"));
+  });
+}
 
-}); 
+
+// Start server after DB connection
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server started on PORT:", PORT);
+  });
 });
-
-
-//app.get("/api/notes", (req,res) => {
-    // send the notes 
-  //  res.status(200).end("Namaste Bharat");
-//});
-
-//app.post("/api/notes", (req,res) => {
-    //create a note 
-  //  res.status(201).json({message:"your note has been Created"});
-//});
-
-//app.put("/api/notes/:id", (req,res) => {
-    //create a note 
-  //  res.status(201).json({message:"your note has been Created"});
-//});
-
-// localhost:5001/api/21
-
-//app.delete("/api/notes/:id", (req,res) => {
-    //create a note 
-  //  res.status(201).json({message:"your note has been deleted"});
-//});
-
-
-
-
-
